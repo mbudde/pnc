2b64d7f265eb981211e236f2facca84043dd9152
Michael Budde <mbudde@gmail.com>
2016-01-13 19:46:34 +0100

    Read prelude from users home directory

--- src/main.rs
+++ src/main.rs
@@ -15,14 +15,25 @@ fn main() {
     env_logger::init().unwrap();
     let mut calc = calc::Calc::new();
 
-    let mut prelude_file = File::open("prelude.pnc").unwrap();
-    let mut prelude = String::new();
-    prelude_file.read_to_string(&mut prelude).unwrap();
-
-    let prelude_words = shlex::Shlex::new(&prelude);
+    let builtin_prelude = include_str!("../prelude.pnc");
+    let prelude_words = shlex::Shlex::new(&builtin_prelude);
     if let Err(err) = calc.run(prelude_words) {
-        println!("Error while executing prelude: {}", err);
+        println!("Error while executing builtin prelude: {}", err);
         std::process::exit(1);
+    }
+
+    if let Some(mut p) = std::env::home_dir() {
+        p.push(".prelude.pnc");
+        if let Ok(mut prelude_file) = File::open(p) {
+            let mut prelude = String::new();
+            prelude_file.read_to_string(&mut prelude).unwrap();
+
+            let prelude_words = shlex::Shlex::new(&prelude);
+            if let Err(err) = calc.run(prelude_words) {
+                println!("Error while executing prelude: {}", err);
+                std::process::exit(1);
+            }
+        }
     }
 
     let mut args = std::env::args();
